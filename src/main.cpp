092c1ed89f4d2bbfcbf4135c3ed8d8326cb795f8
Jagriti gupta <jagriti.gupta_cs.aiml24@gla.ac.in>
2026-02-19 15:05:38 +0530

    Change author and date in main.cpp documentation
    Updated author and date information in the documentation.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,24 @@
  * into a percentage (0–100%) to represent light intensity.
  * The values are displayed on the Serial Monitor.
  *
- * @author Bhartendu Ji
- * @date 18 Feb 2026
+ * @author Jagriti Gupta
+ * @date 19 Feb 2026
  */
+#include <Arduino.h>
+
+#define ldrpin A0   ///< LDR sensor connected to analog pin A0
+
+
+/**
+ * @brief Initializes serial communication and LDR pin.
+ *
+ * This function runs once when the Arduino starts.
+ * - Starts Serial communication at 9600 baud rate
+ * - Sets LDR pin as INPUT
+ * - Prints setup confirmation message
+ */
+void setup() {
+    Serial.begin(9600);
+    pinMode(ldrpin, INPUT);
+    Serial.println("LDR setup");
+}
